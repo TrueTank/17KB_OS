ede24eaaee00540c3c5adfeece1f4f92fbee3c42
Anuar Sabirov <AnuarSab@yandex.kz>
2019-03-22 04:08:44 +0500

    Worked with it, done it, but not at all, done 4 tasks

--- Sabirov/sheal.sh
+++ Sabirov/sheal.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 echo User name is
 whoami
+echo --------------------------------
 echo Memory usage
 free --mega
+echo --------------------------------
+echo Volume of disk space
+df -m
+echo --------------------------------
+echo Proccesses 
+ps -a
+echo --------------------------------
 sleep 5s
