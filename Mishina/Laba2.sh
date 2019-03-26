ee499e4bf2625ff472a14a88762a5e341d442548
Nadeha <miscina.nadyu>
2019-03-27 00:20:07 +0500

    character has been replaced by $

--- Mishina/Laba2.sh
+++ Mishina/Laba2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 select var in "Current user" "Amount of memory used" "Disk space" "Running processes" "Running processes by the current user" "System date and time" "System startup time" "Exit"
 do 
-echo Svar;
-case "Svar" in
+echo $var;
+case "$var" in
 "Current user") whoami;;
 "Amount of memory used") free -m;; 
 "Disk space") df -k;; 
