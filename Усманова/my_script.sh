74e77537c69893dec3615feb02fa2fc53235c1a0
Vixinsens <46163954+Vixinsens@users.noreply.github.com>
2019-03-27 10:49:35 +0300

    working code

--- Усманова/my_script.sh
+++ Усманова/my_script.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+number=0
+while [ $number -ne 8 ]
+do
 echo "1 - get username"
 echo "2 - get memory usege"
 echo "3 - get volume of a disk space"
@@ -8,3 +10,17 @@ echo "5 - get users processers"
 echo "6 - get date"
 echo "7 - get time"
 echo "8 - exit"
+
+read number
+echo "--------------------------"
+
+case "$number" in
+1 ) whoami;;
+2 ) free;;
+3 ) df;;
+4 ) ps -a;;
+5 ) ps -u;;
+6 ) date;;
+7 ) uptime;;
+esace
+done
