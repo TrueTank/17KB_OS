1f72f57c3530a74c41d4f95080c644aa99d1774d
Vixinsens <46163954+Vixinsens@users.noreply.github.com>
2019-03-27 10:47:43 +0300

    menu

---
+++ Усманова/my_script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "1 - get username"
+echo "2 - get memory usege"
+echo "3 - get volume of a disk space"
+echo "4 - get processers"
+echo "5 - get users processers"
+echo "6 - get date"
+echo "7 - get time"
+echo "8 - exit"
